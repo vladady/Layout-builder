d0a8cd34b3aa084f7de44dab5cd8fc38b5b0d2d5
Vlad Buda <vb@cylex.ro>
2014-04-07 12:51:08 +0300

    Panes

--- plugins/display_renderers/layout_builder_editor.class.php
+++ plugins/display_renderers/layout_builder_editor.class.php
@@ -35,18 +35,16 @@ class layout_builder_editor extends panels_renderer_ipe {
   function prepare($external_settings = NULL) {
     $this->prep_run = TRUE;
   }*/
-  function render_pane_content(&$pane) {
-    $current_display = &drupal_static('current_display');
-
-    if(empty($current_display)) {
-      $current_display = $this;
-    }
-    dpm($current_display);
-    parent::render_pane_content($pane);
-  }
-
   function ajax_add_pane($region = NULL, $type_name = NULL, $subtype_name = NULL, $step = NULL) {
-      //dpm(func_get_args());
+      if($type_name == 'layout_builder_regions') {
+          $this->prepare();
+      }
+      
+      $current_display = &drupal_static('current_display');
+      if(empty($current_display)) {
+          $current_display = $this;
+      }
+
       parent::ajax_add_pane($region, $type_name, $subtype_name, $step);
   }
 
